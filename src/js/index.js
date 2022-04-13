5dcc75c500c6abf57e223d6466df88c72ef7c153
Kellyson <Kellysondias@outlook.com>
2022-04-13 11:54:52 -0300

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,7 @@ const caixaDeTexto = document.querySelectorAll('.caixa-de-texto')
 
 caixaDeTexto.forEach(texto => {
     texto.addEventListener('click', () => {
-        textoAtivo = document.querySelector
-        ('.texto-ativo')
+        textoAtivo = document.querySelector('.texto-ativo')
         textoAtivo ? texto.classList.remove('texto-ativo') : texto.classList.add('texto-ativo')
     })
 })
